a1ca6341ecd89f80e9a30047d01990e8c7483f4e
Ali Helmi <ahelmi365@gmail.com>
2023-08-18 12:59:11 +0300

    updted the data of the main table

--- src/components/myRequests/MyRequests.tsx
+++ src/components/myRequests/MyRequests.tsx
@@ -6,15 +6,20 @@ import "datatables.net-dt";
 import "datatables.net-dt/css/jquery.dataTables.css";
 import "./style.css";
 
+function createNestedTable(rowData: any) {
+  // console.log({rowData})
+  const html = '<table id="' + rowData.id.replace(" ", "-") + '">';
+  return html;
+}
 const MyRequests = () => {
   const mytableRef = useRef<any>(null);
   const [showNewChild, setShowNewChild] = useState("");
   const rowData = [
     {
-      id: "t-1",
-      name: "John Doe",
-      age: 30,
-      city: "New York",
+      id: "Req-1",
+      lastModifiedDate: "12/10/2020",
+      activeQRCode: "Yes",
+      lastRequestStatus: "Accpeted",
       transactions: [
         { trId: "1", date: "10/10/2020", status: "pending" },
         { trId: "2", date: "11/10/2020", status: "rejected" },
@@ -22,10 +27,10 @@ const MyRequests = () => {
       ],
     },
     {
-      id: "t-2",
-      name: "Jane Smith",
-      age: 25,
-      city: "Los Angeles",
+      id: "Req-2",
+      lastModifiedDate: "12/10/2020",
+      activeQRCode: "No",
+      lastRequestStatus: "Rejected",
       transactions: [
         { trId: "1", date: "10/10/2020", status: "pending" },
         { trId: "2", date: "11/10/2020", status: "rejected" },
@@ -33,10 +38,10 @@ const MyRequests = () => {
       ],
     },
     {
-      id: "t-3",
-      name: "Bob Johnson",
-      age: 40,
-      city: "Chicago",
+      id: "Req-3",
+      lastModifiedDate: "12/10/2020",
+      activeQRCode: "No",
+      lastRequestStatus: "Rejected",
       transactions: [
         { trId: "1", date: "10/10/2020", status: "pending" },
         { trId: "2", date: "11/10/2020", status: "rejected" },
@@ -46,69 +51,63 @@ const MyRequests = () => {
   ];
 
   const columns = [
-    { title: "ID", data: "id" },
-    { title: "Name", data: "name" },
-    { title: "Age", data: "age" },
-    { title: "City", data: "city" },
+    { title: "Request ID", data: "id" },
+    { title: "Last Modifie dDate", data: "lastModifiedDate" },
+    { title: "Active QR Code?", data: "activeQRCode" },
+    { title: "Last Request Status", data: "lastRequestStatus" },
   ];
 
-  const rows = rowData.map((item, index) => {
-    return (
-      <tr key={index}>
-        <td>{item.name}</td>
-        <td>{item.age}</td>
-        <td>{item.city}</td>
-      </tr>
-    );
-  });
-
+  // useeffct to show nested table (must be before the main useeffect to draw main table)
   useEffect(() => {
     // Add event listener for opening and closing first level childdetails
     const mytable = mytableRef.current;
-    $("#my-requests-table tbody").on(
-      "click",
-      "td.details-control",
-      function (e) {
-        let tr = $(this).closest("tr");
+    $("#my-requests-table tbody").on("click", "td.details-control", (e) => {
+      // let tr = $(this).closest("tr");
+      const tr = $(e.currentTarget).closest("tr");
 
-        let row = mytable.row(tr);
+      const row = mytable.row(tr);
 
-        let rowData = row.data();
+      const rowData = row.data();
 
-        if (row.child.isShown()) {
-          // This row is already open - close it
-          row.child.hide();
-          tr.removeClass("shown");
+      if (row.child.isShown()) {
+        // This row is already open - close it
+        row.child.hide();
+        tr.removeClass("shown");
 
-          //   Destroy the Child Datatable
-          $("#" + rowData.id.replace(" ", "-"))
-            .DataTable()
-            .destroy();
-        } else {
-          // Open this row
-          row.child(format(rowData)).show();
-          var id = rowData.id.replace(" ", "-");
-          //   setShowNewChild(id);
-          const childTable = $("#" + id);
-          console.log({ id });
-          console.log({ childTable });
+        //   Destroy the Child Datatable
+        $("#" + rowData.id.replace(" ", "-"))
+          .DataTable()
+          .destroy();
+      } else {
+        // Open this row
+        row.child(createNestedTable(rowData)).show();
+        const id = rowData.id.replace(" ", "-");
+        //   setShowNewChild(id);
 
-          $("#" + id).DataTable({
-            dom: "t",
-            data: rowData.transactions,
-            columns: [
-              { data: "trId", title: "ID" },
-              { data: "date", title: "Date" },
-              { data: "status", title: "Status" },
-            ],
-            scrollY: "100px",
-          });
+        $("#" + id).DataTable({
+          dom: "t",
+          data: rowData.transactions,
+          columns: [
+            {
+              className: "nested-table-column-1",
+              orderable: false,
+              data: null,
+              defaultContent: "",
+              width: "2.25rem",
+            },
+            { data: "trId", title: "ID" },
+            { data: "date", title: "Date" },
+            { data: "status", title: "Status" },
+          ],
+          scrollY: "100px",
+        });
 
-          tr.addClass("shown");
-        }
+        tr.addClass("shown");
       }
-    );
-  }, [showNewChild]);
+    });
+  }, []);
+
+  // useeffect to draw the main table
   useEffect(() => {
     const mytable = $("#my-requests-table").DataTable({
       data: rowData,
@@ -153,44 +152,6 @@ const MyRequests = () => {
     });
     mytableRef.current = mytable;
   }, [rowData]);
-  //   function format(rowData: any) {
-  //     var html = '<table id="' + rowData.id.replace(" ", "-") + '">';
-  //     html += "<thead><tr><th>ID</th><th>Date</th><th>Status</th></tr></thead>";
-  //     html +=
-  //       "<tbody><tr><td>" +
-  //       rowData.transactions.id +
-  //       "</td><td>" +
-  //       rowData.transactions.date +
-  //       "</td><td>" +
-  //       rowData.transactions.status +
-  //       "</td></tr></tbody>";
-  //     html += "</table>";
-  //     return html;
-  //   }
-
-  function format(rowData: any) {
-    // console.log({rowData})
-    var html = '<table id="' + rowData.id.replace(" ", "-") + '">';
-    // console.log({ html });
-    // html += "<thead><tr><th>ID</th><th>Date</th><th>Status</th></tr></thead>";
-    // html += "<tbody>";
-
-    // rowData.transactions.forEach((transaction: any) => {
-    //   //   console.log({ transaction });
-    //   html +=
-    //     "<tr><td>" +
-    //     transaction.trId +
-    //     "</td><td>" +
-    //     transaction.date +
-    //     "</td><td>" +
-    //     transaction.status +
-    //     "</td></tr>";
-    // });
-
-    // html += "</tbody></table>";
-    // console.log({ html });
-    return html;
-  }
 
   return (
     <div className="main">

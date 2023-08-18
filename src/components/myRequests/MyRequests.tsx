202a2a60a81cbf36c1f63d8a695deaf8c88e0e66
Ali Helmi <ahelmi365@gmail.com>
2023-08-18 12:10:04 +0300

    working nested child in datatable

--- src/components/myRequests/MyRequests.tsx
+++ src/components/myRequests/MyRequests.tsx
@@ -1,34 +1,52 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import $ from "jquery";
+import "jquery";
 import "datatables.net";
 import "datatables.net-dt";
 import "datatables.net-dt/css/jquery.dataTables.css";
+import "./style.css";
 
 const MyRequests = () => {
- 
-
+  let mytable: any;
+  const [showNewChild, setShowNewChild] = useState("");
   const rowData = [
-    { name: "John Doe", age: 30, city: "New York" },
-    { name: "Jane Smith", age: 25, city: "Los Angeles" },
-    { name: "Bob Johnson", age: 40, city: "Chicago" },
-    { name: "John Doe", age: 30, city: "New York" },
-    { name: "Jane Smith", age: 25, city: "Los Angeles" },
-    { name: "Bob Johnson", age: 40, city: "Chicago" },
-    { name: "John Doe", age: 30, city: "New York" },
-    { name: "Jane Smith", age: 25, city: "Los Angeles" },
-    { name: "Bob Johnson", age: 40, city: "Chicago" },
-    { name: "John Doe", age: 30, city: "New York" },
-    { name: "Jane Smith", age: 25, city: "Los Angeles" },
-    { name: "Bob Johnson", age: 40, city: "Chicago" },
-    { name: "John Doe", age: 30, city: "New York" },
-    { name: "Jane Smith", age: 25, city: "Los Angeles" },
-    { name: "Bob Johnson", age: 40, city: "Chicago" },
-    { name: "John Doe", age: 30, city: "New York" },
-    { name: "Jane Smith", age: 25, city: "Los Angeles" },
-    { name: "Bob Johnson", age: 40, city: "Chicago" },
+    {
+      id: "t-1",
+      name: "John Doe",
+      age: 30,
+      city: "New York",
+      transactions: [
+        { trId: "1", date: "10/10/2020", status: "pending" },
+        { trId: "2", date: "11/10/2020", status: "rejected" },
+        { trId: "3", date: "12/10/2020", status: "accepted" },
+      ],
+    },
+    {
+      id: "t-2",
+      name: "Jane Smith",
+      age: 25,
+      city: "Los Angeles",
+      transactions: [
+        { trId: "1", date: "10/10/2020", status: "pending" },
+        { trId: "2", date: "11/10/2020", status: "rejected" },
+        { trId: "3", date: "12/10/2020", status: "accepted" },
+      ],
+    },
+    {
+      id: "t-3",
+      name: "Bob Johnson",
+      age: 40,
+      city: "Chicago",
+      transactions: [
+        { trId: "1", date: "10/10/2020", status: "pending" },
+        { trId: "2", date: "11/10/2020", status: "rejected" },
+        { trId: "3", date: "12/10/2020", status: "accepted" },
+      ],
+    },
   ];
 
   const columns = [
+    { title: "ID", data: "id" },
     { title: "Name", data: "name" },
     { title: "Age", data: "age" },
     { title: "City", data: "city" },
@@ -43,12 +61,69 @@ const MyRequests = () => {
       </tr>
     );
   });
+
   useEffect(() => {
-    $("#my-requests-table").DataTable({
+    // Add event listener for opening and closing first level childdetails
+    $("#my-requests-table tbody").on(
+      "click",
+      "td.details-control",
+      function (e) {
+        let tr = $(this).closest("tr");
+
+        let row = mytable.row(tr);
+
+        let rowData = row.data();
+
+        if (row.child.isShown()) {
+          // This row is already open - close it
+          row.child.hide();
+          tr.removeClass("shown");
+
+          //   Destroy the Child Datatable
+          $("#" + rowData.id.replace(" ", "-"))
+            .DataTable()
+            .destroy();
+        } else {
+          // Open this row
+          row.child(format(rowData)).show();
+          var id = rowData.id.replace(" ", "-");
+        //   setShowNewChild(id);
+          const childTable = $("#" + id);
+          console.log({ id });
+          console.log({ childTable });
+
+          $("#" + id).DataTable({
+            dom: "t",
+            data: rowData.transactions,
+            columns: [
+              { data: "trId", title: "ID" },
+              { data: "date", title: "Date" },
+              { data: "status", title: "Status" },
+            ],
+            scrollY: "100px",
+          });
+
+          tr.addClass("shown");
+        }
+      }
+    );
+  }, [showNewChild]);
+  useEffect(() => {
+    mytable = $("#my-requests-table").DataTable({
       data: rowData,
-      columns:columns,
+      columns: [
+        {
+          className: "details-control",
+          orderable: false,
+          data: null,
+          defaultContent: "",
+        },
+
+        ...columns,
+      ],
       destroy: true,
       paging: true,
+      pageLength: 5,
       searching: true,
       retrieve: true,
       ordering: true,
@@ -75,12 +150,51 @@ const MyRequests = () => {
         },
       ],
     });
-  }, []);
+  }, [rowData]);
+  //   function format(rowData: any) {
+  //     var html = '<table id="' + rowData.id.replace(" ", "-") + '">';
+  //     html += "<thead><tr><th>ID</th><th>Date</th><th>Status</th></tr></thead>";
+  //     html +=
+  //       "<tbody><tr><td>" +
+  //       rowData.transactions.id +
+  //       "</td><td>" +
+  //       rowData.transactions.date +
+  //       "</td><td>" +
+  //       rowData.transactions.status +
+  //       "</td></tr></tbody>";
+  //     html += "</table>";
+  //     return html;
+  //   }
+
+  function format(rowData: any) {
+    // console.log({rowData})
+    var html = '<table id="' + rowData.id.replace(" ", "-") + '">';
+    // console.log({ html });
+    // html += "<thead><tr><th>ID</th><th>Date</th><th>Status</th></tr></thead>";
+    // html += "<tbody>";
+
+    // rowData.transactions.forEach((transaction: any) => {
+    //   //   console.log({ transaction });
+    //   html +=
+    //     "<tr><td>" +
+    //     transaction.trId +
+    //     "</td><td>" +
+    //     transaction.date +
+    //     "</td><td>" +
+    //     transaction.status +
+    //     "</td></tr>";
+    // });
+
+    // html += "</tbody></table>";
+    // console.log({ html });
+    return html;
+  }
+
   return (
     <div className="main">
       <div className="container">
         <div className="row">
-          <table id="my-requests-table" ></table>
+          <table id="my-requests-table"></table>
         </div>
       </div>
     </div>
